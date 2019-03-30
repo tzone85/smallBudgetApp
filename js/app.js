e5f020a17ca9cfe53d79b9a28c8e93ff814832c2
tzone85 <tzone85@gmail.com>
2019-03-30 20:41:37 +0200

    did the event listenners, mostly for the budget form and then some work on the cosmetics, colour changes depending on the inputed data. Also worked on small error hendling with a 4secs timout.

--- js/app.js
+++ js/app.js
@@ -15,4 +15,78 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+
+  // submit budget method
+  submitBudgetForm(){
+    const value = this.budgetInput.value;
+    if (value === '' || value < 0 ){
+      this.budgetFeedback.classList.add('showItem');
+      this.budgetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;
+
+      const self = this;
+      setTimeout(function () {
+        self.budgetFeedback.classList.remove('showItem');
+      }, 4000);
+    } else {
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = '';
+      this.showBalance()
+    }
+  }
+
+  // show balance
+  showBalance(){
+    const expense = this.totalExpense();
+    const total = parseInt(this.budgetAmount.textContent) - expense;
+    this.balanceAmount.textContent = total;
+
+    if (total < 0){
+      this.balance.classList.remove('showGreen', 'showBlack');
+      this.balance.classList.add('showRed');
+    }
+    else if (total > 0){
+      this.balance.classList.remove('showRed', 'showGreen');
+      this.balance.classList.add('showGreen');
+
+    }
+    else if (total === 0){
+      this.balance.classList.remove('showGreen', 'showRed');
+      this.balance.classList.add('showBlack');
+    }
+  }
+
+  // total expense
+  totalExpense(){
+    let total = 400;
+    return total;
+  }
 }
+
+function eventListeners(){
+  const budgetForm = document.getElementById('budget-form');
+  const expenseForm = document.getElementById('expense-form');
+  const expenseList = document.getElementById('expense-list');
+
+  // new instance of the UI Class
+  const ui = new UI();
+
+  // budget form submit
+  budgetForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    ui.submitBudgetForm();
+  });
+
+  // expense form submit
+  expenseForm.addEventListener('submit', function(event){
+    event.preventDefault();
+  });
+
+  // expense click
+  expenseList.addEventListener('click', function(event){
+
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  eventListeners();
+});
