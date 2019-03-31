b69226a4b0b67b96a525d270cec45f36087ebdd1
tzone85 <tzone85@gmail.com>
2019-03-31 14:10:50 +0200

    added functionality for expenses. From the form to showing the correct balances, error handling as well as changing it to use ZAR currency

--- js/app.js
+++ js/app.js
@@ -55,9 +55,75 @@ class UI {
     }
   }
 
+  // submit expense form
+  submitExpenseForm(){
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+
+    if (expenseValue === '' || amountValue === '' || amountValue < 0){
+      this.expenseFeedback.classList.add('showItem');
+      this.expenseFeedback.innerHTML = `<p>values cannot be empty or negative</p>`;
+
+      const self = this;
+      setTimeout(function () {
+        self.expenseFeedback.classList.remove('showItem');
+      }, 4000);
+    }
+    else {
+      let amount = parseInt(amountValue);
+      this.expenseInput.value = "";
+      this.amountInput.value = "";
+
+      let expense = {
+        id:       this.itemID,
+        title:    expenseValue,
+        amount:   amount
+      };
+
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      this.showBalance();
+    }
+  }
+
+  // add expense
+  addExpense(expense){
+    const div = document.createElement('div');
+    div.classList.add('expense');
+    div.innerHTML = `
+    <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+         <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+         <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>
+`;
+    this.expenseList.appendChild(div);
+  }
+
   // total expense
   totalExpense(){
-    let total = 400;
+    let total = 0;
+
+    if (this.itemList.length > 0){
+      total = this.itemList.reduce(function (accumilator, current) {
+        // console.log(`Total is ${accumilator} and the current value is ${current.amount}`);
+        accumilator += current.amount;
+        return accumilator;
+      }, 0);
+    }
+
+    this.expenseAmount.textContent = total;
     return total;
   }
 }
@@ -79,6 +145,7 @@ function eventListeners(){
   // expense form submit
   expenseForm.addEventListener('submit', function(event){
     event.preventDefault();
+    ui.submitExpenseForm();
   });
 
   // expense click
